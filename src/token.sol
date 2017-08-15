44619c065823a2f3dd779d2e73041782d688c285
Mariano Conti <nanexcool@gmail.com>
2017-08-15 13:17:57 -0300

    rely/deny

--- src/token.sol
+++ src/token.sol
@@ -17,6 +17,8 @@ import "./base.sol";
 
 contract DSToken is DSTokenBase(0), DSStop {
 
+    uint256 constant MAX_UINT = 2**256 - 1;
+
     bytes32  public  symbol;
     uint256  public  decimals = 18; // standard token precision. override to customize
 
@@ -27,10 +29,20 @@ contract DSToken is DSTokenBase(0), DSStop {
     function transfer(address dst, uint wad) stoppable note returns (bool) {
         return super.transfer(dst, wad);
     }
-    function transferFrom(
-        address src, address dst, uint wad
-    ) stoppable note returns (bool) {
-        return super.transferFrom(src, dst, wad);
+    function transferFrom(address src, address dst, uint wad)
+        stoppable note returns (bool) {
+        require(_balances[src] >= wad);
+        require(_approvals[src][msg.sender] >= wad);
+
+        if (_approvals[src][msg.sender] < MAX_UINT) {
+            _approvals[src][msg.sender] = sub(_approvals[src][msg.sender], wad);
+        }
+        _balances[src] = sub(_balances[src], wad);
+        _balances[dst] = add(_balances[dst], wad);
+
+        Transfer(src, dst, wad);
+
+        return true;
     }
     function approve(address guy, uint wad) stoppable note returns (bool) {
         return super.approve(guy, wad);
@@ -50,6 +62,12 @@ contract DSToken is DSTokenBase(0), DSStop {
     function burn(uint128 wad) auth stoppable note {
         _balances[msg.sender] = sub(_balances[msg.sender], wad);
         _supply = sub(_supply, wad);
+    }
+    function rely(address guy) stoppable note returns (bool) {
+        return super.approve(guy, MAX_UINT);
+    }
+    function deny(address guy) stoppable note returns (bool) {
+        return super.approve(guy, 0);
     }
 
     // Optional token name
