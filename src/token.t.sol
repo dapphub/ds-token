8955a8d26d2cbb481d2b0f5181fc747bb280e705
Nikolai Mushegian <nikolai@nexusdev.us>
2017-08-23 16:28:26 -0400

    begin consolidating tests

--- src/token.t.sol
+++ src/token.t.sol
@@ -22,38 +22,33 @@ contract TokenUser {
         token = token_;
     }
 
-    function doTransferFrom(address from, address to, uint amount)
+    function doMove(address from, address to, uint amount)
         returns (bool)
     {
         return token.move(from, to, amount);
     }
 
-    function doTransfer(address to, uint amount)
+    function doPush(address to, uint amount)
         returns (bool)
     {
         return token.push(to, amount);
     }
 
-    function doApprove(address recipient, uint amount)
+    function doRely(address recipient)
         returns (bool)
     {
         return token.approve(recipient, amount);
     }
 
-    function doAllowance(address owner, address spender)
-        constant returns (uint)
+    function doDeps(address owner, address spender)
+        constant returns (bool)
     {
         return token.allowance(owner, spender);
     }
 
-    function doBalanceOf(address who) constant returns (uint) {
+    function doBals(address who) constant returns (uint) {
         return token.balanceOf(who);
     }
-
-    function doSetName(bytes32 name) constant {
-        token.setName(name);
-    }
-
 }
 
 contract DSTokenTest is DSTest {
@@ -117,7 +112,7 @@ contract DSTokenTest is DSTest {
     function testChargesAmountApproved() logs_gas {
         uint amountApproved = 20;
         token.approve(user2, amountApproved);
-        assert(user2.doTransferFrom(this, user2, amountApproved));
+        assert(user2.doMove(this, user2, amountApproved));
         assertEq(token.balanceOf(this), initialBalance - amountApproved);
     }
 
@@ -161,5 +156,120 @@ contract DSTokenTest is DSTest {
         user1.doSetName("Test");
     }
 
+}
+
+contract TokenUser {
+    ERC20  token;
+
+    function TokenUser(ERC20 token_) {
+        token = token_;
+    }
+
+    function doTransferFrom(address from, address to, uint amount)
+        returns (bool)
+    {
+        return token.transferFrom(from, to, amount);
+    }
+
+    function doTransfer(address to, uint amount)
+        returns (bool)
+    {
+        return token.transfer(to, amount);
+    }
+
+    function doApprove(address recipient, uint amount)
+        returns (bool)
+    {
+        return token.approve(recipient, amount);
+    }
+
+    function doAllowance(address owner, address spender)
+        constant returns (uint)
+    {
+        return token.allowance(owner, spender);
+    }
+
+    function doBalanceOf(address who) constant returns (uint) {
+        return token.balanceOf(who);
+    }
+}
+
+contract DSTokenBaseTest is DSTest {
+    uint constant initialBalance = 1000;
+
+    ERC20 token;
+    TokenUser user1;
+    TokenUser user2;
+
+    function setUp() {
+        token = createToken();
+        user1 = new TokenUser(token);
+        user2 = new TokenUser(token);
+    }
+
+    function createToken() internal returns (ERC20) {
+        return new DSTokenBase(initialBalance);
+    }
+
+    function testSetupPrecondition() {
+        assertEq(token.balanceOf(this), initialBalance);
+    }
+
+    function testTransferCost() logs_gas() {
+        token.transfer(address(0), 10);
+    }
+
+    function testAllowanceStartsAtZero() logs_gas {
+        assertEq(token.allowance(user1, user2), 0);
+    }
+
+    function testValidTransfers() logs_gas {
+        uint sentAmount = 250;
+        log_named_address("token11111", token);
+        token.transfer(user2, sentAmount);
+        assertEq(token.balanceOf(user2), sentAmount);
+        assertEq(token.balanceOf(this), initialBalance - sentAmount);
+    }
+
+    function testFailWrongAccountTransfers() logs_gas {
+        uint sentAmount = 250;
+        token.transferFrom(user2, this, sentAmount);
+    }
+
+    function testFailInsufficientFundsTransfers() logs_gas {
+        uint sentAmount = 250;
+        token.transfer(user1, initialBalance - sentAmount);
+        token.transfer(user2, sentAmount+1);
+    }
+
+
+    function testApproveSetsAllowance() logs_gas {
+        log_named_address("Test", this);
+        log_named_address("Token", token);
+        log_named_address("Me", this);
+        log_named_address("User 2", user2);
+        token.approve(user2, 25);
+        assertEq(token.allowance(this, user2), 25);
+    }
+
+    function testChargesAmountApproved() logs_gas {
+        uint amountApproved = 20;
+        token.approve(user2, amountApproved);
+        assert(user2.doTransferFrom(this, user2, amountApproved));
+        assertEq(token.balanceOf(this), initialBalance - amountApproved);
+    }
+
+    function testFailTransferWithoutApproval() logs_gas {
+        address self = this;
+        token.transfer(user1, 50);
+        token.transferFrom(user1, self, 1);
+    }
+
+    function testFailChargeMoreThanApproved() logs_gas {
+        address self = this;
+        token.transfer(user1, 50);
+        user1.doApprove(self, 20);
+        token.transferFrom(user1, self, 21);
+    }
 }
 
